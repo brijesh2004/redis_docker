96c975bfb12fee886886bcd19b68abe019778c22
brijesh2004 <brskumar0102@gmail.com>
2025-04-20 14:55:56 +0530

    Setup the production for redis

--- redisClient.js
+++ redisClient.js
@@ -3,11 +3,10 @@ const { createClient } = require('redis');
 
 // Use service name "redis" from docker-compose as host
 const redisClient = createClient({
-  url:'redis://default:VcKPWFEN28Z8ffa6i3luYHovMmt2DWsy@redis-15177.c321.us-east-1-2.ec2.redns.redis-cloud.com:15177'
+  url: process.env.REDIS_URL || 'redis://localhost:6379'
 });
 
-
-console.log("redisClient hai" , process.env.REDIS_URL);
+console.log("redisClient hai ji" , process.env.REDIS_URL);
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
